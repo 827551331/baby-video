76039798271a8641e1a45ba8051e878a2d8ef14c
wangyiwei <wdssdream@163.com>
2019-01-16 17:42:52 +0800

    init
    init

---
+++ src/views/mu-dialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <mu-container>
+      <mu-flex justify-content="center">
+        <mu-button @click="openSimpleDialog">Open Dialog</mu-button>
+      </mu-flex>
+      <mu-dialog title="Dialog" width="360" :open.sync="openSimple">this is simple Dialog
+        <mu-button slot="actions" flat color="primary" @click="closeSimpleDialog">Close</mu-button>
+      </mu-dialog>
+    </mu-container>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      openSimple: false
+    };
+  },
+  methods: {
+    openSimpleDialog() {
+      this.openSimple = true;
+    },
+    closeSimpleDialog() {
+      this.openSimple = false;
+    }
+  }
+};
+</script>
